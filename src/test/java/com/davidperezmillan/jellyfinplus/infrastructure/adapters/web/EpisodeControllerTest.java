d454700bc24635285c5794c2dd3847df6e045773
David Perez Millan <david.perez.millan@gmail.com>
2025-12-07 13:29:49 +0100

    Update Spring Boot to version 3.4.0 and replace deprecated @MockBean with @MockitoBean in tests

--- src/test/java/com/davidperezmillan/jellyfinplus/infrastructure/adapters/web/EpisodeControllerTest.java
+++ src/test/java/com/davidperezmillan/jellyfinplus/infrastructure/adapters/web/EpisodeControllerTest.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -21,7 +21,7 @@ class EpisodeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private EpisodeService episodeService;
 
     @Autowired
