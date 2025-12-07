e3e356052b48a1e886f46007b01e1c231e96697c
David Perez Millan <david.perez.millan@gmail.com>
2025-12-07 13:07:45 +0100

    Fix integration test to use correct Jellyfin configuration with @TestPropertySource

--- src/test-it/java/com/davidperezmillan/jellyfinplus/JellyfinIntegrationTest.java
+++ src/test-it/java/com/davidperezmillan/jellyfinplus/JellyfinIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
@@ -13,6 +14,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test") // If needed, but since .env is used, perhaps not
+@TestPropertySource(properties = {
+    "jellyfin.baseUrl=http://play.davidperezmillan.com/",
+    "jellyfin.token=ee4bf04cad094a7b80d54adc8d7ae4ec",
+    "jellyfin.userName=apps"
+})
 class JellyfinIntegrationTest {
 
     @Autowired
